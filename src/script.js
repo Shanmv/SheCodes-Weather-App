ae52d26df1c70efdc70cbea47da3774b86c0b1c8
Shanice Valentine <shanmv13@gmail.com>
2022-04-03 00:37:48 -0400

    Added loop for days of the week in 7 day forecast

--- src/script.js
+++ src/script.js
@@ -32,28 +32,37 @@ function formatDate(currentDate) {
   let amPm = hour >= 12 ? `PM` : `AM`;
   let twelveHr = hour % 12 || 12;
   let minute = currentDate.getMinutes();
-  minute = minute <= 9 ? "0" + minute : minute;
+  minute = minute <= 9 ? `0` + minute : minute;
   let formattedDate = `${day}, ${month} ${date}, ${year} ${twelveHr}:${minute} ${amPm}`;
   return formattedDate;
 }
 let today = formatDate(new Date());
-let h2 = document.querySelector("h2");
+let h2 = document.querySelector(`h2`);
 h2.innerHTML = `${today}`;
-
+//days of the week for forecast
+function getLabel(day) {
+  let forecastDay = [`Sun`, `Mon`, `Tues`, `Wed`, `Thurs`, `Fri`, `Sat`];
+  for (let i = 0; i < forecastDay.length; i++) {
+    let name = forecastDay[(day.getDay() + i) % 7];
+    let dayName = document.querySelectorAll(`.wk-Day`);
+    dayName[i].innerHTML = `${name}`;
+  }
+}
+getLabel(new Date());
 //import weather
-let apiKey = "eab52e4b15607908ae3212e851e07600";
+let apiKey = `eab52e4b15607908ae3212e851e07600`;
 
 //Search City
 function search(event) {
   event.preventDefault();
-  let city = document.querySelector(".bar");
-  let location = document.querySelector("h1");
+  let city = document.querySelector(`.bar`);
+  let location = document.querySelector(`h1`);
   location.innerHTML = `${city.value}`;
   let geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location.innerHTML}&appid=${apiKey}`;
   axios.get(`${geoUrl}`).then(getCoordinates);
 }
-let form = document.querySelector(".search-bar");
-form.addEventListener("submit", search);
+let form = document.querySelector(`.search-bar`);
+form.addEventListener(`submit`, search);
 
 function getCoordinates(response) {
   let latitude = response.data[0].lat;
@@ -65,22 +74,22 @@ function getCoordinates(response) {
 function displayTemp(response) {
   console.log(response.data);
   let currentTemp = Math.round(response.data.current.temp);
-  let trueTemp = document.querySelector("#today-wx");
+  let trueTemp = document.querySelector(`#today-wx`);
   trueTemp.innerHTML = `${currentTemp}°`;
   //precipitation
   let precip = Math.round(response.data.daily[0].pop);
-  let chancePrecip = document.querySelector("#percentPrecip");
+  let chancePrecip = document.querySelector(`#percentPrecip`);
   chancePrecip.innerHTML = `${precip}%`;
   //humidity
   let percentHumidity = response.data.current.humidity;
-  let Humidity = document.querySelector("#percentHum");
+  let Humidity = document.querySelector(`#percentHum`);
   Humidity.innerHTML = `${percentHumidity}%`;
   //windspeed
   let wind = Math.round(response.data.current.wind_speed);
-  let windspeed = document.querySelector("#windspeed");
+  let windspeed = document.querySelector(`#windspeed`);
   windspeed.innerHTML = `${wind}km/h`;
   //description
   let description = response.data.current.weather[0].description;
-  let weather = document.querySelector("h4");
+  let weather = document.querySelector(`h4`);
   weather.innerHTML = `${description}`;
 }
