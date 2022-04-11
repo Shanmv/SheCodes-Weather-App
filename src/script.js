29ed81e4378e69eccb3b0296fb1e44f822c836b4
Shanice Valentine <shanmv13@gmail.com>
2022-04-10 22:06:45 -0400

    changed syntax, updated daily forecast loop

--- src/script.js
+++ src/script.js
@@ -1,27 +1,27 @@
 //Date for h2
 function formatDate(currentDate) {
   let days = [
-    `Sunday`,
-    `Monday`,
-    `Tuesday`,
-    `Wednesday`,
-    `Thursday`,
-    `Friday`,
-    `Saturday`,
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
   ];
   let months = [
-    `January`,
-    `February`,
-    `March`,
-    `April`,
-    `May`,
-    `June`,
-    `July`,
-    `August`,
-    `September`,
-    `October`,
-    `November`,
-    `December`,
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
   let day = days[currentDate.getDay()];
@@ -29,74 +29,78 @@ function formatDate(currentDate) {
   let date = currentDate.getDate();
   let year = currentDate.getFullYear();
   let hour = currentDate.getHours();
-  let amPm = hour >= 12 ? `PM` : `AM`;
+  let amPm = hour >= 12 ? "PM" : "AM";
   let twelveHr = hour % 12 || 12;
   let minute = currentDate.getMinutes();
-  minute = minute <= 9 ? `0` + minute : minute;
+  minute = minute <= 9 ? "0" + minute : minute;
   let formattedDate = `${day}, ${month} ${date}, ${year} ${twelveHr}:${minute} ${amPm}`;
   return formattedDate;
 }
 let today = formatDate(new Date());
-let h2 = document.querySelector(`h2`);
+let h2 = document.querySelector("h2");
 h2.innerHTML = `${today}`;
-//days of the week for forecast
+//loop days of the week for forecast
 function getLabel(day) {
-  let forecastDay = [`Sun`, `Mon`, `Tues`, `Wed`, `Thurs`, `Fri`, `Sat`];
+  let forecastDay = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   for (let i = 0; i < forecastDay.length; i++) {
     let name = forecastDay[(day.getDay() + i) % 7];
-    let dayName = document.querySelectorAll(`.wk-Day`);
+    let dayName = document.querySelectorAll(".wk-Day");
     dayName[i].innerHTML = `${name}`;
   }
 }
 getLabel(new Date());
 //import weather
-let apiKey = `eab52e4b15607908ae3212e851e07600`;
+let apiKey = "eab52e4b15607908ae3212e851e07600";
 
 //Search City
 function search(event) {
   event.preventDefault();
-  let city = document.querySelector(`.bar`);
-  let location = document.querySelector(`h1`);
+  let city = document.querySelector(".bar");
+  let location = document.querySelector("h1");
   location.innerHTML = `${city.value}`;
   let geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${location.innerHTML}&appid=${apiKey}`;
-  axios.get(`${geoUrl}`).then(getCoordinates);
+  axios.get(geoUrl).then(getCoordinates);
 }
-let form = document.querySelector(`.search-bar`);
-form.addEventListener(`submit`, search);
+let form = document.querySelector(".search-bar");
+form.addEventListener("submit", search);
 
 function getCoordinates(response) {
   let latitude = response.data[0].lat;
   let longitude = response.data[0].lon;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&limit=3&units=metric&appid=${apiKey}`;
-  axios.get(`${apiUrl}`).then(displayTemp);
+  axios.get(apiUrl).then(displayTemp);
 }
 
 function displayTemp(response) {
   console.log(response.data);
   let currentTemp = Math.round(response.data.current.temp);
-  let trueTemp = document.querySelector(`#today-wx`);
+  let trueTemp = document.querySelector("#today-wx");
   trueTemp.innerHTML = `${currentTemp}°`;
+  
   //precipitation
   let precip = Math.round(response.data.daily[0].pop);
-  let chancePrecip = document.querySelector(`#percentPrecip`);
+  let chancePrecip = document.querySelector("#percentPrecip");
   chancePrecip.innerHTML = `${precip}%`;
   //humidity
   let percentHumidity = response.data.current.humidity;
-  let Humidity = document.querySelector(`#percentHum`);
+  let Humidity = document.querySelector("#percentHum");
   Humidity.innerHTML = `${percentHumidity}%`;
   //windspeed
   let wind = Math.round(response.data.current.wind_speed);
-  let windspeed = document.querySelector(`#windspeed`);
+  let windspeed = document.querySelector("#windspeed");
   windspeed.innerHTML = `${wind}km/h`;
   //description
   let description = response.data.current.weather[0].description;
-  let weather = document.querySelector(`h4`);
+  let weather = document.querySelector("h4");
   weather.innerHTML = `${description}`;
-
+  //loop through daily array
   for (let i = 0; i < 7; i++) {
     let forecastTempHigh = Math.round(response.data.daily[i].temp.max);
     let forecastTempLow = Math.round(response.data.daily[i].temp.min);
-    let dailyTemp = document.querySelectorAll(`.wx-forecast`);
-    dailyTemp[i].innerHTML = `${forecastTempHigh}° | ${forecastTempLow}°`;
+    let dailyTempHigh = document.querySelectorAll(".tempHigh");
+    let dailyTempLow = document.querySelectorAll(".tempLow");
+    dailyTempHigh[i].innerHTML = `${forecastTempHigh}° |`;
+    dailyTempLow[i].innerHTML = ` ${forecastTempLow}°`;
+   
   }
 }
